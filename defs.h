8be31014aa13fcaef51252e6298692c9fb248aab
Joon Lim <joondlim@gmail.com>
2016-03-31 20:14:57 -0400

    Add random.c with funtion random(), which returns a random uint between 0 and ((2^32 - 1) / 2), which is 2147483647. The algorithm for the function was taken from http://stackoverflow.com/questions/1167253/implementation-of-rand.

--- defs.h
+++ defs.h
@@ -179,5 +179,8 @@ void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
 
+// random.c
+uint            random(void);
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
